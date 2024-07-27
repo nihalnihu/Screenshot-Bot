05070ad32a0bd48687dd4f18794d16da97e52188
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-27 21:52:48 +0530

    Update screenshotbot.py

--- bot/screenshotbot.py
+++ bot/screenshotbot.py
@@ -6,17 +6,22 @@ class ScreenShotBot(Client):
 
     def __init__(self):
         super().__init__(
-            name=Config.SESSION_NAME,  # Use session_name here
+            session_name="screenshotbot",
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             workers=20,
             plugins=dict(
                 root="bot/plugins"
-            )
+            ),
+            proxy={
+                "scheme": "http",
+                "hostname": "localhost",
+                "port": 8000
+            }
         )
 
-        self.db = Database(Config.DATABASE_URL, Config.SESSION_NAME)
+        self.db = Database(Config.DATABASE_URL, "screenshotbot")
         self.CURRENT_PROCESSES = {}
         self.CHAT_FLOOD = {}
         self.broadcast_ids = {}
