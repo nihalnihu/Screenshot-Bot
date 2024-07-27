aaae986b5fcd167589ccd705759843d4fa8d4ac8
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-27 17:19:32 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,16 +1,6 @@
-from pyrogram.errors import FloodWait
-import time
 from .screenshotbot import ScreenShotBot
 
 app = ScreenShotBot()
 
 if __name__ == "__main__":
-    while True:
-        try:
-            app.run()
-        except FloodWait as e:
-            print(f"Flood wait error: {e.x} seconds. Waiting...")
-            time.sleep(e.x)
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}")
-            break  # Break the loop or handle other errors as needed
+    app.run()
