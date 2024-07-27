ca6a9edfb5274cb497fcb0723864f113623ec805
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-27 22:26:36 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,28 @@
 from .screenshotbot import ScreenShotBot
+from flask import Flask
+import threading
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+bot = Flask(__name__)
+
+@bot.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@bot.route('/health')
+def health_check():
+    return 'Healthy', 200
+
+def run_flask():
+    bot.run(host='0.0.0.0', port=8080)
 
 app = ScreenShotBot()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    threading.Thread(target=run_flask).start()
+    
+    # Start the Pyrogram Client
     app.run()
