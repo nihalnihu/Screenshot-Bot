e25f705eed6173609945d05e0f420a09e35acfe3
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-27 16:39:00 +0530

    Update screenshotbot.py

--- bot/screenshotbot.py
+++ bot/screenshotbot.py
@@ -6,7 +6,7 @@ class ScreenShotBot(Client):
 
     def __init__(self):
         super().__init__(
-            session="sample_bot",  # Use "session" for the parameter name
+            session_name="sample_bot",  # Use session_name here
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
